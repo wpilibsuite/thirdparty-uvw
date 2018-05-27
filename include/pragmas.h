cb014192013de67bcc43c67538996e6e4f73cafd
Thad House <ThadHouse@users.noreply.github.com>
2018-05-27 10:05:30 -0700

    Gets cross platform build working
    Separate source sets for linux, mac and windows

---
+++ include/pragmas.h
@@ -0,0 +1,8 @@
+#pragma once
+#pragma comment(lib, "Advapi32.lib")
+#pragma comment(lib, "IPHLPAPI.lib")
+#pragma comment(lib, "Psapi.lib")
+#pragma comment(lib, "Userenv.lib")
+#pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "User32.lib")
